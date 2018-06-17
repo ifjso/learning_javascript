fee2975710ac7f5e08e13d0191b99615f40f6c9d
JesusOh <ojsboy@hotmail.com>
2018-06-17 18:27:36 +0900

    chapter11 completed

---
+++ es6/chapter11/11_1_ErrorObject.js
@@ -0,0 +1,14 @@
+function validateEmail(email) {
+    return email.match(/@/) ?
+        email :
+        new Error(`invalid email: ${email}`);
+}
+
+const email = "jane@doe.com";
+
+const validatedEmail = validateEmail(email);
+if (validatedEmail instanceof Error) {
+    console.error(`Error: ${validatedEmail.message}`);
+} else {
+    console.log(`Valid email: ${validatedEmail}`);
+}
