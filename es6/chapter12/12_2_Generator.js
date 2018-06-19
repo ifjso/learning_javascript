fe0ff1b9b1db93ac8ca7725a141f4736c08b4fe5
JesusOh <ojsboy@hotmail.com>
2018-06-19 17:18:29 +0900

    chapter12 completed

---
+++ es6/chapter12/12_2_Generator.js
@@ -0,0 +1,23 @@
+function* rainbow() {
+    yield 'red';
+    yield 'orange';
+    yield 'yellow';
+    yield 'green';
+    yield 'blue';
+    yield 'indigo';
+    yield 'violet';
+}
+
+const it = rainbow();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+for (let color of rainbow()) {
+    console.log(color);
+}
