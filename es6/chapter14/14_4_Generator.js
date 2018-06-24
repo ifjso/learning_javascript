8901c93415b37e2ef9e4c6b75870869c1b71e84c
JesusOh <ojsboy@hotmail.com>
2018-06-24 15:15:35 +0900

    Added Promise.all() and try/catch

--- es6/chapter14/14_4_Generator.js
+++ es6/chapter14/14_4_Generator.js
@@ -35,11 +35,26 @@ function grun(g) {
 }
 
 function* theFutureIsNow() {
-    const dataA = yield nfcall(fs.readFile, 'a.txt');
-    const dataB = yield nfcall(fs.readFile, 'b.txt');
-    const dataC = yield nfcall(fs.readFile, 'c.txt');
+    let data;
+    try {
+        data = yield Promise.all([
+            nfcall(fs.readFile, 'a.txt'),
+            nfcall(fs.readFile, 'b.txt'),
+            nfcall(fs.readFile, 'c.txt'),
+        ]);
+    } catch(err) {
+        console.error("Unable to read one or more input files: " + err.message);
+        throw err;
+    }
+
     yield ptimeout(5 * 1000);
-    yield nfcall(fs.writeFile, 'd.txt', dataA + dataB + dataC);
+
+    try {
+        yield nfcall(fs.writeFile, 'd.txt', data[0] + data[1] + data[2]);
+    } catch(err) {
+        console.error("Unable to write output file: " + err.message);
+        throw err;
+    }
 }
 
 grun(theFutureIsNow);
