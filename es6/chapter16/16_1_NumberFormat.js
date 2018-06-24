847a4ed8a46699ae4c60ffdde3bbba8d615311b2
JesusOh <ojsboy@hotmail.com>
2018-06-24 18:53:09 +0900

    Complted chapter16

---
+++ es6/chapter16/16_1_NumberFormat.js
@@ -0,0 +1,39 @@
+let x = 19.51;
+console.log('toFixed()');
+console.log(x.toFixed(3));
+console.log(x.toFixed(2));
+console.log(x.toFixed(1));
+console.log(x.toFixed(0));
+
+x = 3800.5;
+console.log('\ntoExponential()');
+console.log(x.toExponential(4));
+console.log(x.toExponential(3));
+console.log(x.toExponential(2));
+console.log(x.toExponential(1));
+console.log(x.toExponential(0));
+
+x = 1000;
+console.log('\ntoPrecision()');
+console.log(x.toPrecision(5));
+console.log(x.toPrecision(4));
+console.log(x.toPrecision(3));
+console.log(x.toPrecision(2));
+console.log(x.toPrecision(1));
+
+x = 15.335;
+console.log('\ntoPrecision()');
+console.log(x.toPrecision(6));
+console.log(x.toPrecision(5));
+console.log(x.toPrecision(4));
+console.log(x.toPrecision(3));
+console.log(x.toPrecision(2));
+console.log(x.toPrecision(1));
+
+x = 12;
+console.log('\ntoString()');
+console.log(x.toString());
+console.log(x.toString(10));
+console.log(x.toString(16));
+console.log(x.toString(8));
+console.log(x.toString(2));
