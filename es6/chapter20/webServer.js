ae68fc36e31be9129a41f9b4cb83abe213f9afa2
JesusOh <ojsboy@hotmail.com>
2018-07-13 18:00:24 +0900

    Completed 20.10 web server

---
+++ es6/chapter20/webServer.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer(function(req, res) {
+    if (req.method === 'GET' && req.url === '/favicon.ico') {
+        console.log('ddd');
+        const fs = require('fs');
+        const rs = fs.createReadStream('favicon.ico');
+        rs.pipe(res);
+    } else {
+        console.log(`${req.method} ${req.url}`);
+        res.end('Hello world!');
+    }
+});
+
+const port = 8080;
+server.listen(port, function() {
+    console.log(`server started on port ${port}`);
+});
